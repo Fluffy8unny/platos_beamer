50107315608a12e1ae7053ed132139468ecf663e
fluffybunny <fllfybnny@gmail.com>
2025-09-01 22:20:20 +0200

    fixed lock

--- devenv.nix
+++ devenv.nix
@@ -1,24 +1,37 @@
-{ pkgs, lib, config, ... }:
+{ inputs, pkgs, lib, config, ... }:
+  let
+          #links libraries to shell
+    libPath = pkgs.lib.makeLibraryPath [
+            pkgs-stable.git
+            pkgs.clang
+            pkgs.opencv
+            pkgs.libclang    
+            pkgs.libglvnd
+            pkgs.wayland
+            pkgs.glxinfo
+            pkgs.libxkbcommon
+          ];
+  pkgs-stable = import inputs.nixpkgs-stable { system = pkgs.stdenv.system; };
+  in
 {
-  languages = { 
-        rust = { enable = true; }; 
-        c.enable = true; 
+  languages = {
+        rust = { enable = true; };
+        c.enable = true;
         };
-
   packages = [
+    pkgs-stable.git
     pkgs.clang
     pkgs.opencv
-    pkgs.libclang
-    pkgs.xorg.xauth
-    pkgs.xorg.libX11
-    pkgs.xorg.libXcursor
-    pkgs.xorg.libXi
-    pkgs.libxkbcommon
+    pkgs.libclang    
     pkgs.libglvnd
+    pkgs.wayland
+    pkgs.glxinfo
+    pkgs.libxkbcommon
   ];
 
   git-hooks.hooks.clippy.enable = true;
   git-hooks.hooks.rustfmt.enable = true;
   env.LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-  env.LD_LIBRARY_PATH="${pkgs.libglvnd}/lib:${pkgs.libxkbcommon}/lib:$LD_LIBRARY_PATH";
+  env.LD_LIBRARY_PATH = libPath;
+  enterShell = " glxinfo -B; ";
 }
